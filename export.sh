78040b8d355ef563c5456d7c71ac8ff83c66894a
David LJ <mail@davidlj95.com>
2017-04-28 02:15:59 +0200

    Export scripts

---
+++ export.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Name: export
+# Description: Exports the JSON resume using JSONResume command line tools
+#              https://jsonresume.org
+
+# Constants
+RESUME_FILE="resume.json"
+EXPORTS_FOLDER="./docs"
+EXPORTS=("english.pdf" "index.html")
+THEME="elegant"
+
+# Create exports folder
+mkdir "$EXPORTS_FOLDER" &> /dev/null
+
+# Compile
+for file in ${EXPORTS[@]}; do
+	echo $file
+	resume export --theme $THEME "$EXPORTS_FOLDER/$file"
+done
