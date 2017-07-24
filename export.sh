e712b82a80f36cb58a1667e2a8bd178e50f027ae
David LJ <mail@davidlj95.com>
2017-07-24 23:04:08 +0200

    Added automatic PDF generation (fixed one) Corrected some typos

--- export.sh
+++ export.sh
@@ -2,11 +2,17 @@
 # Name: export
 # Description: Exports the JSON resume using JSONResume command line tools
 #              https://jsonresume.org
-
+# Usage: ./export.sh
+# If you want to save a PDF file, please previously export a HTML to
+# convert HTML to PDF properly (JSONResume sucks at this feature)
+#
+# Dependencies:
+# You need wkhtmltopdf and http-server (NodeJS)
 # Constants
 RESUME_FILE="resume.json"
 EXPORTS_FOLDER="./docs"
-EXPORTS=("index.html")
+EXPORTS=("index.html" "davidlj95_Resume.pdf")
+LATEST_HTML=""
 THEME="elegant"
 
 # Create exports folder
@@ -15,9 +21,23 @@ mkdir "$EXPORTS_FOLDER" &> /dev/null
 # Compile
 for file in ${EXPORTS[@]}; do
 	echo Exporting $file
-	resume export --theme $THEME "$EXPORTS_FOLDER/$file"
+	file_dest="$EXPORTS_FOLDER/$file"
+	# HTML
 	if [[ "$file" == *html ]]; then
+		resume export --theme $THEME "$file_dest"
 		echo - Fixing HTML
-		./fix_html.sh "$EXPORTS_FOLDER/$file"
+		./fix_html.sh "$file_dest"
+		LATEST_HTML="$file_dest"
+	fi
+	# PDF
+	if [[ "$file" == *pdf ]]; then
+		echo - Converting HTML to PDF
+		echo - Launching webserver
+		http-server $(dirname "$LATEST_HTML") &
+		pid_http=$!
+		sleep 2
+		wkhtmltopdf --print-media-type --zoom 1.25 http://localhost:8080 "$EXPORTS_FOLDER/$file"
+		kill $pid_http
+		echo - Done
 	fi
 done
