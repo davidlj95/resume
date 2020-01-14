4c1c184dd762b6c589c3523e8a1854555e7fa1c5
David LJ <mail@davidlj95.com>
2020-01-14 02:29:27 +0100

    feat: 2020 resume update
    use jsonresume-theme-material-design
    manually fix HTML to include icons
    manually created PDF due to wkhtml2pdf issues

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# resume
-My professional career resume (curriculum vitae) written using JSONResume
+# Resume
+My professional career resume (curriculum vitae) written using JSONResume and a [self-made Material Design theme](https://github.com/davidlj95/jsonresume-theme-material-design)
--- export.sh
+++ export.sh
@@ -8,35 +8,35 @@
 #
 # Dependencies:
 # You need wkhtmltopdf and http-server (NodeJS)
+set -e
+
 # Constants
 RESUME_FILE="resume.json"
 EXPORTS_FOLDER="./docs"
 EXPORTS=("index.html" "davidlj95_Resume.pdf")
 LATEST_HTML=""
-THEME="elegant"
+THEME="material-design"
 
 # Create exports folder
-mkdir "$EXPORTS_FOLDER" &> /dev/null
+mkdir -p "$EXPORTS_FOLDER" &> /dev/null
 
 # Compile
-for file in ${EXPORTS[@]}; do
+for file in "${EXPORTS[@]}"; do
 	echo Exporting $file
 	file_dest="$EXPORTS_FOLDER/$file"
 	# HTML
 	if [[ "$file" == *html ]]; then
-		resume export --theme $THEME "$file_dest"
-		echo - Fixing HTML
-		./fix_html.sh "$file_dest"
+		npx resume export --theme $THEME "$file_dest"
 		LATEST_HTML="$file_dest"
 	fi
 	# PDF
 	if [[ "$file" == *pdf ]]; then
 		echo - Converting HTML to PDF
 		echo - Launching webserver
-		http-server $(dirname "$LATEST_HTML") &
+		npx http-server "$(dirname "$LATEST_HTML")" &
 		pid_http=$!
 		sleep 2
-		wkhtmltopdf --print-media-type --zoom 1.25 http://localhost:8080 "$EXPORTS_FOLDER/$file"
+		wkhtmltopdf --print-media-type --zoom 1 http://localhost:8080 "$EXPORTS_FOLDER/$file"
 		kill $pid_http
 		echo - Done
 	fi
